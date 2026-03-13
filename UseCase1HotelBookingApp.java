0eac8b9d6a71e95f65d3524279423b9428f71d9a
NiranjanaKrishnaK <niranjanakumar2510@gmail.com>
2026-03-13 09:46:48 +0530

    Added Use Case 1: Application Entry & Welcome Message

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,19 @@
+/**
+ * UseCase1HotelBookingApp
+ * -----------------------
+ * Application Entry & Welcome Message
+ *
+ * This class demonstrates the entry point of the Hotel Booking Management System.
+ * It prints a welcome message and confirms successful startup.
+ *
+ * @author Developer
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay App!");
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("Application started successfully.");
+    }
+}
